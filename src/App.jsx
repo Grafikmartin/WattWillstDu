ea108e9446172e09c364c18b0d3d76c2dc2e6143
dci-student <grafikmartin@gmail.com>
2025-02-26 15:29:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";  // Import der CSS-Datei
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <h1>WattWillstDu</h1>
+    </div>
+  );
+}
